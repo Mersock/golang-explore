46f0b775f8d5543083754f714ae31db912c242b8
Knight <knzphumthawan@gmail.com>
2020-10-30 21:29:30 +0700

    add test case

--- make-go-doc/mersockacdc/main_test.go
+++ make-go-doc/mersockacdc/main_test.go
@@ -1,9 +1,18 @@
 package mersockacdc
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExampleSum() {
 	fmt.Println(Sum(2, 3))
 	//Output:
 	//5
+}
+
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum(i, i)
+	}
 }
